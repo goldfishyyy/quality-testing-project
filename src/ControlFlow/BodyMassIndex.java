dbb854ec29405b8a67a35ed64b7bb9f449127c14
Csongor Farago <csongor.farago@sjsu.edu>
2019-09-20 18:59:14 -0700

    BMI Code Finish/Polish/Comment

--- src/ControlFlow/BodyMassIndex.java
+++ src/ControlFlow/BodyMassIndex.java
@@ -1,58 +1,110 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class BodyMassIndex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		String output = "";
-		double weight;
-		double height;
-		
-		for(int i = 1; i < 10; i++)
-		{
-			System.out.println("Enter the data for person " + i);
-			weight = 0;
-			height = 0;
-			while(weight == 0)
-			{
-				System.out.print("\tEnter weight in kilograms: ");
-				try
-				{weight = input.nextDouble();}
-				catch(Exception e)
-				{
-					System.out.println("\tNot a valid input");
-					weight = 0;
-					input.nextLine();
-				}
-				//input.nextLine();
-			}
-			
-			while(height == 0)
-			{
-				System.out.print("\tEnter height in meters: ");
-				try
-				{height = input.nextDouble();}
-				catch(Exception e)
-				{
-					System.out.println("\tNot a valid input");
-					height = 0;
-					input.nextLine();
-				}
-				//input.nextLine();
-			}
-			
-			weight *= 2.20462;
-			height *= 39.3701;
-			output = output.concat(("BMI for person " + i + ": " + BMI(weight, height)+ "\n"));
-		}
-		input.close();
-		System.out.println(output);
-		
+            ArrayList<Person> people = new ArrayList<Person>();
+            
+            //Geathering information about people
+            Scanner input = new Scanner(System.in);             
+            for(int i = 1; i < 10; i++){
+                Person p = new Person(0, 0);                       
+		System.out.println("Enter the data for person " + i);
+		while(p.weight == 0){
+                    System.out.print("\tEnter weight in kilograms: ");
+                    try{
+                        p.weight = input.nextDouble();
+                    }catch(Exception e){
+                        System.out.println("\tNot a valid input");
+                        p.weight = 0;
+                        input.nextLine();
+                    }
+                }			
+		while(p.height == 0){
+                    System.out.print("\tEnter height in meters: ");
+                    try{
+                        p.height = input.nextDouble();
+                    }catch(Exception e){
+                        System.out.println("\tNot a valid input");
+			p.height = 0;
+			input.nextLine();
+                    }
+                }              
+                people.add(p);
+            }
+            input.close();
+            
+            //Calculate mean for all people
+            double mean = CalculateMean(people);
+            System.out.println("\n");
+            System.out.println("BMI mean for all people:  " + mean);
+            
+            //Printing people info
+            //printin people above the mean (skipping people who <= mean)
+            System.out.println("People above the mean:");
+            for(int i = 0; i < people.size(); i++){   
+                if(people.get(i).bmi() <= mean) continue;
+                System.out.print("\t");
+                System.out.print("BMI for person "+i+": "+people.get(i).bmi());
+                System.out.print(", Fall in range: "+people.get(i).range());
+                System.out.println(", Above the mean!");
+            }
+            //printin people bellow the mean (skipping people who > mean)
+            System.out.println("People bellow the mean:");
+            for(int i = 0; i < people.size(); i++){   
+                if(people.get(i).bmi() > mean) continue;
+                System.out.print("\t");
+                System.out.print("BMI for person "+i+": "+people.get(i).bmi());
+                System.out.print(", Fall in range: "+people.get(i).range());
+                System.out.println(", Bellow the mean!");
+            }            	
 	}
-	public static double BMI(double weight, double height)
-	{
-		return ( weight * 703.0) / Math.pow(height, 2);
+        
+        // Method used to run trough the array of people and determine the mean BMI
+	public static double CalculateMean(ArrayList<Person> people){
+            double sum = 0;                     
+            for(int i = 0; i < people.size(); i++){
+                sum += people.get(i).bmi();
+            }          
+            return sum / people.size();
 	}
+	
+	//This class is used to hold data on a person and calculate his/her BMI and BMIRange
+        public static class Person {
+            
+            public double weight;
+            public double height;                      
+            
+            //Constructor
+            public Person(double weight, double height){
+                this.weight = weight;
+                this.height = height;               
+            }
+            
+        
+            // Method to Calculate BMI with given weigh/height numbers
+            public double bmi(){
+                return weight / Math.pow(height, 2);
+            }
+            
+            // Method to Calculate BMI with new weigh/height numbers
+            public double bmi(double weight, double height){
+                this.weight = weight;
+                this.height = height;                   
+                return bmi();
+            }
+            
+            
+            // Method to determen what range is the person fall into
+            public String range(){
+                double bmi = this.bmi();
+                if(bmi < 18.5) { return "Underweight"; }
+                else if(bmi >= 18.5 && bmi < 25) { return "Normal"; } 
+                else if(bmi >= 25 && bmi < 30) { return "Overweight"; } 
+                else { return "Obese"; }            
+            }            
+        }
+
 }
