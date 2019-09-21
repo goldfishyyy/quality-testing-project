59536da20443ad4da743459adaa246141c6bec19
Chrisrdouglas <20939800+Chrisrdouglas@users.noreply.github.com>
2019-09-20 19:20:21 -0700

    changed everything to print out entire sequence

--- src/ControlFlow/Fibonacci.java
+++ src/ControlFlow/Fibonacci.java
@@ -3,15 +3,45 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		recursiveFibonacci(7);
+		recursiveFibonacci(11);
+		recursiveFibonacci(0);
 	}
 	
-	public static int recursiveFibonacci(int n)
+	/*
+	 * This is the entry point to print the Fibonacci numbers.
+	 * The function takes an integer n which determines the number of
+	 * Fibonacci numbers to print out. if n is greater than zero then it prints the first
+	 * number in the sequence and calls the fib() method passing it the first two numbers in the sequence
+	 * and n-1. Finally this method will end the line with a \n character and return to the caller.
+	 * */
+	public static void recursiveFibonacci(int n)
 	{
-		if(n <= 1)//if n == 0 or n == 1 then return n
+		if(n > 0)
 		{
-			return n;
+			System.out.print("0");
+			fib(0, 1, n-1);
+			System.out.print('\n');
 		}
-		return recursiveFibonacci(n-1) + recursiveFibonacci(n-1);
 	}
+	
+	/*
+	 * This method will check to see if n==0 and return without printing out any
+	 * further Fibonacci numbers. If there are more numbers to print it will continue to
+	 * call itself decrementing n each time.
+	 * */
+	public static void fib(int a, int b, int n)
+	{
+		if(n == 0)// if i is the nth fib number then we're done
+		{
+			return;
+		}
+		else
+		{
+			System.out.print(", "+b);	//print the ", " to make the output look organized and readable
+			fib(b, a+b, n-1);			//a+b will be the next number in the sequence and the n-1 is what will eventually terminate the recursive calls
+		}
+	}
+	
+	
 }
